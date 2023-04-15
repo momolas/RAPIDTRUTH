ac7bcd7351f6f28a64ac5ac3aef5669183507db6
Momo L'As <momolas@users.noreply.github.com>
2023-04-15 18:19:11 +0200

    Initial Commit

---
+++ RAPIDTRUTH/RAPIDTRUTHApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAPIDTRUTHApp.swift
+//  RAPIDTRUTH
+//
+//  Created by Mo on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct RAPIDTRUTHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
