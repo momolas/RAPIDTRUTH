379fe6dd3450897c5ff15d3265429b0996b89ded
Momo L'As <momolas@users.noreply.github.com>
2026-08-29 21:02:53 +0200

    chore: clean up build artifacts in SwiftVehicleProtocols package directory

--- Packages/SwiftVehicleProtocols/Sources/SwiftVehicleProtocols/SwiftVehicleProtocols.swift
+++
@@ -1,6 +0,0 @@
-@_exported import VehicleCore
-@_exported import VehicleISOTP
-@_exported import VehicleDiagnostic
-@_exported import VehicleTransport
-@_exported import VehicleTransportPanda
-@_exported import VehicleAnalytics
